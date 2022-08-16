23f8b3fef5881529c29862b59ccc709d52c1bf87
dk <denis.kolokolov@icloud.com>
2022-08-16 16:50:14 +0200

    finished the whole back-end

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :tasks
+  # # list all tasks
+  # get 'tasks', to: 'tasks#index'
+  # # add a new task
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
+  # # view the details of a task
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # # edit a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # remove a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
